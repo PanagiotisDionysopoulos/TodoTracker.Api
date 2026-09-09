42f5fd096d9c5c8a7cee802dce27cb1595fbb5f7
ΠΑΝΑΓΙΩΤΗΣ ΔΙΟΝΥΣΟΠΟΥΛΟΣ <cs151008@uniwa.gr>
2026-09-09 15:11:11 +0300

    Add ITodoService, TodoService and register Dependency Injection

---
+++ TodoTracker.Api/Services/ITodoService.cs
@@ -0,0 +1,13 @@
+﻿using TodoTracker.Api.Models;
+
+namespace TodoTracker.Api.Services
+{
+    public interface ITodoService
+    {
+        List <TodoItem> GetAll ();
+        TodoItem? GetById (int id);
+        TodoItem Create(string title);
+        bool Update (int id, bool isComplete);
+        bool Delete (int id);
+    }
+}
---
+++ TodoTracker.Api/Services/TodoService.cs
@@ -0,0 +1,50 @@
+﻿using TodoTracker.Api.Models;
+
+namespace TodoTracker.Api.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly List<TodoItem> _todos = new();
+        private int _nextId = 1;
+
+        public List<TodoItem> GetAll() {  return _todos; }
+        public TodoItem GetById(int id)
+        {
+            return _todos.FirstOrDefault(t => t.Id == id);
+        }
+        public TodoItem Create(string title) {
+            var newTodo = new TodoItem
+            {
+                Id = _nextId++,
+                Title = title,
+                IsCompleted = false,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _todos.Add(newTodo);
+            return newTodo;
+        }
+        public bool Update(int id, bool isCompleted)
+        {
+            var todo = GetById(id);
+            if (todo == null)
+            {
+                return false;
+            }
+
+            todo.IsCompleted = isCompleted;
+            return true;
+        }
+        public bool Delete(int id) {
+            var todo = GetById(id);
+            if (todo == null)
+            {
+                return false;
+            }
+
+            _todos.Remove(todo);
+            return true;
+        }
+
+    }
+}
